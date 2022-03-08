9d210df85fac7fb038b52811afa81f3ba5511a2e
Glenda Sabino <95448881+glendasabinoo@users.noreply.github.com>
2022-03-08 14:58:46 -0300

    exercise 19

--- aula19/Exercicio19.java
+++ aula19/Exercicio19.java
@@ -8,33 +8,41 @@ public class Exercicio19 {
 		Scanner scan = new Scanner(System.in);
 		double[] notas1 = new double [10];
 		double[] notas2 = new double [notas1.length];
-		double media = 0;
 		double[] result = new double [notas1.length];
 		
 		
 		for(int i=0; i<notas1.length;i++) {
 			System.out.println("Informe o valor da primeira nota bimestral do aluno " + (i+1));
 			notas1[i] = scan.nextInt();
-		}
-		
-		for(int i=0; i<notas1.length;i++) {
+			
 			System.out.println("Informe o valor da segunda nota bimestral do aluno " + (i+1));
 			notas2[i] = scan.nextInt();
+			
+			result[i] = (notas1[i] + notas2[i] / 2);
 		}
 		
-		for(int i=0;i<notas1.length;i++) {
-			media = (notas1[i] + notas2[i]) / 2;
-			System.out.println("Média do aluno " + (i+1) + ": " + media);
-			
-			if(media >= 7) {
-				System.out.println("aprovado!");
-			} else {
-				System.out.println("reprovado!");
-			}
-		}
-	
+		System.out.print("Notas 1 = ");
+        for (int i=0; i<notas1.length; i++){
+            System.out.print(notas1[i] + " ");
+        }
+        System.out.println();
+        
+        System.out.print("Notas 2 = ");
+        for (int i=0; i<notas2.length; i++){
+            System.out.print(notas2[i] + " ");
+        }
+        System.out.println();
+        
+        System.out.println("Resultados:");
+        for (int i=0; i<result.length; i++){
+            
+            if (result[i] >= 7){
+                System.out.println(result[i] + " - Aprovado");
+            } else {
+                System.out.println(result[i] + " - Reprovado");
+            }
 		
 
 	}
-
+	}
 }
